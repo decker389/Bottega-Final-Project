681be7e6b81d7d72430e6f718cd6ff24fa13466f
christopher decker <decker389@aol.com>
2021-12-29 22:30:07 -0700

    added image to account Login page and added link to account recovery page. Finished styles for page.

--- src/components/pages/Account/accountLogin/accountLoginForm.js
+++ src/components/pages/Account/accountLogin/accountLoginForm.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { NavLink } from 'react-router-dom';
 
 import { FormInput, FormButton } from '../../../pageComponents/formFields';
+import shelfImg from '../../../../Static/Images/GamingShelf.jpg';
 
 class AccountLoginForm extends Component {
     render() {
@@ -12,9 +14,20 @@ class AccountLoginForm extends Component {
                     <Field className="login-form__password" type="password" title="Password" placeholder="" name="password" component={FormInput} />
                     <Field onClick={() => console.log('That submited')} className='login-button' type="login" title="Login" name='Login' component={FormButton} />
                 </form>
-                <div>
+                <div className='login-page-help-link-wrapper'>
                     <h2>account recovery</h2>
-                    <p>Forgot Password?</p>
+                    <NavLink exact to='/AccountDashboard' className='help-link'>Forgot Password?</NavLink>
+                    <div className='login-image'
+                        style={{
+                            height: '100%',
+                            width: '70%',
+                            background: "url(" + shelfImg + ") no-repeat",
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                            display: 'grid',
+                            alignSelf: 'center'
+                        }}
+                    />
                 </div>
             </div>
         )
